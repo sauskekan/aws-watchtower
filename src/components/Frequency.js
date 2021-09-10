4e2f83ef4835e2d1562fbaea562ae6b18f97562d
saad <saad_wishs@hotmail.com>
2021-09-10 12:11:42 +1000

    adding frequency

---
+++ src/components/Frequency.js
@@ -0,0 +1,7 @@
+const frequency = [
+    { value: "Daily", label: "Daily" },
+    { value: "Weekly", label: "Weekly" },
+    { value: "Monthly", label: "Monthly" }
+  ];
+  
+  export default frequency;
--- src/components/Services.js
+++ src/components/Services.js
@@ -6,6 +6,7 @@ import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from "@material-ui/core/styles";
 import options from "./Options";
+import frequency from "./Frequency";
 import "./App.css";
 
 const styles = {
@@ -32,21 +33,26 @@ class Services extends Component {
     const element = <h1>Please select the services that you would like to receive announcements about them</h1>;
     return (
         element,
-      <><Select
+      <><><Select
             styles={styles}
             closeMenuOnSelect={false}
             isMulti
             options={options}
             defaultValue={options[0]} />
-            <Button color="secondary"
-            variant="contained"
-            onClick={this.back}
-            >Back</Button>
-            <Button color="primary"
-            variant="contained"
-            onClick={this.continue}
-            >Continue</Button>
-            </>
+        </><><Select
+            styles={styles}
+            closeMenuOnSelect={false}
+            options={frequency}
+            defaultValue={frequency[0]} />
+                <Button color="secondary"
+                    variant="contained"
+                    onClick={this.back}
+                >Back</Button>
+                <Button color="primary"
+                    variant="contained"
+                    onClick={this.continue}
+                >Continue</Button>
+            </></>
     )
   }
 }
